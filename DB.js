6df4e1302c9da38b3aa77a5d23bce6f0ce23cc0d
Conor C <Conorc1000@gmail.com>
2015-10-14 12:23:11 +0100

    setup question hashes and retrive questioin hashes function

--- DB.js
+++ DB.js
@@ -3,37 +3,52 @@ var client = redis.createClient(process.env.REDIS_URL, {
   no_ready_check: true
 });
 var db = {};
-//var Qid= 1;//for test
 var aCount = 0;
 var qCount = 0;
+
+
 db.addQHash = function(username, question, date){
   console.log('im in the addQHash');
   qCount ++;
-  client.hmset(Qcount, "username", username, "question", question, "date", date);
+  client.hmset(qCount, "username", username, "question", question, "date", date);
 };
-//db.addQHash(Qid , "username", 'Conor', "question", 'Is this working', "date", '13/10/15');
+
+db.addQHash("conor", "what day is it?", "08/04/14");
+db.addQHash("dan", "what?", "08/05/14");
+db.addQHash("tom", "why?", "08/05/14");
+db.addQHash("bob", "where?", "08/05/14");
+db.addQHash("conor", "what day is it?", "08/04/14");
+db.addQHash("dan", "what?", "08/05/14");
+
+console.log(qCount);
 
 db.addAHash = function(username, answer, date, vote){
   aCount ++;
   client.hmset(aCount , "username", username, "answer", answer, "date", date, "vote", vote);
 };
 
-
-db.lastTenQs = function(date, username, question){
-  client.lrange('Qids', 0, 9, function(err,reply){
-    var frontarr = [];
-    var counter = 0;
-    reply.forEach(client.hgetall(keyName, function(err, obj){
-      frontarr.push(obj);
-    }););
+var QDataArr = [];
+db.lastTenQs = function(qCount){
+  console.log('im in the lastTenQs');
+  var i = qCount;
+  console.log(qCount);
+  //this must happen for each of the last 10 Qs
+  client.multi()
+    .hgetall(i)
+    .hgetall(i-1)
+    .hgetall(i-2)
+    .hgetall(i-3)
+    .hgetall(i-4)
+    .hgetall(i-5)
+    .hgetall(i-6)
+    .hgetall(i-7)
+    .hgetall(i-8)
+    .hgetall(i-9)
+    .exec(function(err, replies){
+        console.log('multi output=', replies);
+        res.write(JSON.stringify(replies));
   });
 };
-
-
-
-
-
-
-
+db.lastTenQs(qCount);
 
 module.exports = db;
